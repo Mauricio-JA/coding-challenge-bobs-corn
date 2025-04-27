bc3934fe13575500f8b6c0aeeb28264aa39c57c7
Mauricio Justiniano <mauricio.justiniano@viacontours.com>
2025-04-27 18:53:39 -0400

    feat: :tada: create vite + react app
    + tailwind & shadcn setup

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl">Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
